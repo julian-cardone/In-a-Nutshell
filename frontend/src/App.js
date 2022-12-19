f6903117742ebe5cb20e734e24f72c36d7fca0b6
Julian Cardone <juliancardone1@gmail.com>
2022-12-19 15:25:13 -0500

    updated appjs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,32 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Calendar from "./components/Calendar";
 
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
+import NavBar from './components/NavBar/NavBar';
+
+import MainPage from './components/MainPage/MainPage';
+import LoginForm from './components/SessionForms/LoginForm';
+import SignupForm from './components/SessionForms/SignupForm';
+
+import { getCurrentUser } from './store/session';
+
 function App() {
-  return (
+  const [loaded, setLoaded] = useState(false);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCurrentUser()).then(() => setLoaded(true));
+  }, [dispatch]);
+
+  return loaded && (
     <>
+      <NavBar />
       <Switch>
-        <Route exact path="/">
-          <h1>Hello from Squirrels</h1>
-        </Route>
+        <AuthRoute exact path="/" component={MainPage} />
+        <AuthRoute exact path="/login" component={LoginForm} />
+        <AuthRoute exact path="/signup" component={SignupForm} />
         <Route exact path="/home">
           <Calendar />
         </Route>
