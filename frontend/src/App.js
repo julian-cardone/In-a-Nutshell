35f73267e2b054f5f820fb5988d81bdf9a765ab2
Julian Cardone <juliancardone1@gmail.com>
2022-12-19 15:33:44 -0500

    updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { Switch } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
 import NavBar from './components/NavBar/NavBar';
@@ -11,6 +11,7 @@ import LoginForm from './components/SessionForms/LoginForm';
 import SignupForm from './components/SessionForms/SignupForm';
 
 import { getCurrentUser } from './store/session';
+import Calendar from './components/Calendar';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -26,6 +27,9 @@ function App() {
         <AuthRoute exact path="/" component={MainPage} />
         <AuthRoute exact path="/login" component={LoginForm} />
         <AuthRoute exact path="/signup" component={SignupForm} />
+        <Route exact path="/home">
+          <Calendar />
+        </Route>
       </Switch>
     </>
   );
