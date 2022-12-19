894de05b94ff64f5483d8e9043240f9d0695148f
Ethan Graham <ethangraham43@gmail.com>
2022-12-19 14:38:07 -0500

    add sessionreducer and errorsreducer

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,9 +2,10 @@
 
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import sessionReducer from './session';
 
 const rootReducer = combineReducers({
-  
+  sessionReducer
 });
 
 let enhancer;
