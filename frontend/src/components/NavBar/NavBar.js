7fb7276fd034fd75a5b1f9736bd38e72321de0e3
Ethan Graham <ethangraham43@gmail.com>
2022-12-19 15:38:15 -0500

    remove css from navbar as well

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import './NavBar.css';
 import { logout } from '../../store/session';
 
 function NavBar () {
