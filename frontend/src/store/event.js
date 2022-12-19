314346ea0fe0bd578ccf356c469461923fc5fd44
Ethan Graham <ethangraham43@gmail.com>
2022-12-19 16:25:52 -0500

    add event store

---
+++ frontend/src/store/event.js
@@ -0,0 +1,33 @@
+import jwtFetch from "./jwt";
+import { RECEIVE_USER_LOGOUT  } from "./session";
+
+const RECEIVE_EVENT = "events/RECEIVE_EVENT"
+const RECEIVE_EVENTS = "events/RECEIVE_EVENTS"
+const RECEIVE_EVENT_ERRORS = "events/RECEIVE_EVENT_ERRORS"
+const CLEAR_EVENT_ERRORS = "events/CLEAR_EVENT_ERRORS"
+
+const receiveEvent = event => ({
+    type: RECEIVE_EVENT,
+    event
+})
+
+const receiveEvents = events => ({
+    type: RECEIVE_EVENTS,
+    events
+})
+
+
+const receiveErrors = errors => ({
+    type: RECEIVE_EVENT_ERRORS,
+    errors
+  });
+  
+  export const clearTweetErrors = errors => ({
+      type: CLEAR_EVENT_ERRORS,
+      errors
+  });
+
+  export const fetchEvents = () => async dispatch => {
+
+  }
+
