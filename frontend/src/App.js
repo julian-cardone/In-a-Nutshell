598a9b242986172eedfa542eef2934c6ce60a4a6
Julian Cardone <juliancardone1@gmail.com>
2022-12-19 15:28:52 -0500

    fixing app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,6 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import Calendar from "./components/Calendar";
-
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { Switch } from 'react-router-dom';
 
 import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
 import NavBar from './components/NavBar/NavBar';
@@ -28,12 +25,7 @@ function App() {
         <AuthRoute exact path="/" component={MainPage} />
         <AuthRoute exact path="/login" component={LoginForm} />
         <AuthRoute exact path="/signup" component={SignupForm} />
-        <Route exact path="/home">
-          <Calendar />
-        </Route>
       </Switch>
     </>
   );
 }
-
-export default App;
