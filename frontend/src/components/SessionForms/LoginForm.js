d4fc70dd877c342123f81f68a7bafd71b0d16f52
rtexelm <texelist@gmail.com>
2022-12-20 11:41:36 -0500

    Add Modal provider and Login

--- frontend/src/components/SessionForms/LoginForm.js
+++ frontend/src/components/SessionForms/LoginForm.js
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
+import { login, clearSessionErrors } from "../../store/session";
+import "./LoginForm.css";
 
-import { login, clearSessionErrors } from '../../store/session';
-
-function LoginForm () {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const errors = useSelector(state => state.errors.session);
+function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const errors = useSelector((state) => state.errors.session);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -17,37 +17,39 @@ function LoginForm () {
   }, [dispatch]);
 
   const update = (field) => {
-    const setState = field === 'email' ? setEmail : setPassword;
-    return e => setState(e.currentTarget.value);
-  }
+    const setState = field === "email" ? setEmail : setPassword;
+    return (e) => setState(e.currentTarget.value);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(login({ email, password })); 
-  }
+    dispatch(login({ email, password }));
+  };
 
   return (
-    <form className="session-form" onSubmit={handleSubmit}>
-      <h2>Log In Form</h2>
-      <div className="errors">{errors?.email}</div>
-      <label>
-        <span>Email</span>
-        <input type="text"
-          value={email}
-          onChange={update('email')}
-          placeholder="Email"
-        />
-      </label>
-      <div className="errors">{errors?.password}</div>
-      <label>
-        <span>Password</span>
-        <input type="password"
-          value={password}
-          onChange={update('password')}
-          placeholder="Password"
-        />
-      </label>
+    <form className="form-fields" onSubmit={handleSubmit}>
+      {/* <div className="errors">{errors?.email}</div> */}
+      {/* <label> */}
+      {/* <span>Email</span> */}
       <input
+        type="text"
+        value={email}
+        onChange={update("email")}
+        placeholder="Email"
+      />
+      {/* </label> */}
+      {/* <div className="errors">{errors?.password}</div> */}
+      {/* <label>
+        <span>Password</span> */}
+      <input
+        type="password"
+        value={password}
+        onChange={update("password")}
+        placeholder="Password"
+      />
+      {/* </label> */}
+      <input
+        className={`btnPrimary btn`}
         type="submit"
         value="Log In"
         disabled={!email || !password}
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,19 +1,21 @@
 // store/store.js
 
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import sessionReducer from './session';
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import modalReducer from "./modal";
+import sessionReducer from "./session";
 
 const rootReducer = combineReducers({
-  sessionReducer
+  sessionReducer,
+  modalReducer,
 });
 
 let enhancer;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   enhancer = applyMiddleware(thunk);
 } else {
-  const logger = require('redux-logger').default;
+  const logger = require("redux-logger").default;
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
