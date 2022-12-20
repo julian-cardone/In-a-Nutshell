a1375304693e5e461fe5d763355660e53968309b
Ethan Graham <ethangraham43@gmail.com>
2022-12-20 11:54:27 -0500

    change backend app.js

--- backend/app.js
+++ backend/app.js
@@ -29,6 +29,28 @@ if (!isProduction) {
   app.use(cors());
 }
 
+if (isProduction) {
+  const path = require('path');
+  // Serve the frontend's index.html file at the root route
+  app.get('/', (req, res) => {
+    res.cookie('CSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../frontend', 'build', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  app.use(express.static(path.resolve("../frontend/build")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  app.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('CSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../frontend', 'build', 'index.html')
+    );
+  });
+}
+
 app.use(
   csurf({
     cookie: {
