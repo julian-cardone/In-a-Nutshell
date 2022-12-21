6814480bcbb0b62bfb5705e0ba204156d41e0e86
rtexelm <texelist@gmail.com>
2022-12-20 23:54:00 -0500

    Add font constants to index.css

--- frontend/src/components/SessionForms/SignupForm.js
+++ frontend/src/components/SessionForms/SignupForm.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { signup, clearSessionErrors } from "../../store/session";
+import "./SessionForms.css";
 
 function SignupForm() {
   const [email, setEmail] = useState("");
@@ -61,81 +62,89 @@ function SignupForm() {
   };
 
   return (
-    <form className="session-form" onSubmit={usernameSubmit}>
-      <h2>Sign Up Form</h2>
-      <div className="errors">{errors?.email}</div>
-      <label>
-        <span>Email</span>
+    <div className={`signupContainer`}>
+      <form className={`session-form form-fields`} onSubmit={usernameSubmit}>
+        <div className="errors">{errors?.email}</div>
+        {/* <label>
+          <span>Email</span> */}
         <input
+          className={`loginField`}
           type="text"
           value={email}
           onChange={update("email")}
           placeholder="Email"
         />
-      </label>
-      <div className="errors">{errors?.username}</div>
-      <label>
-        <span>Username</span>
+        {/* </label> */}
+        <div className="errors">{errors?.username}</div>
+        {/* <label>
+          <span>Username</span> */}
         <input
+          className={`loginField`}
           type="text"
           value={username}
           onChange={update("username")}
           placeholder="Username"
         />
-      </label>
-      <label>
-        <span>First Name</span>
+        {/* </label>
+        <label>
+          <span>First Name</span> */}
         <input
+          className={`loginField`}
           type="text"
           value={firstName}
           onChange={update("firstName")}
-          placeholder="firstName"
+          placeholder="First Name"
         />
-      </label>
-      <label>
-        <span>Last Name</span>
+        {/* </label>
+        <label>
+          <span>Last Name</span> */}
         <input
+          className={`loginField`}
           type="text"
           value={lastName}
           onChange={update("lastName")}
-          placeholder="lastName"
+          placeholder="Last Name"
         />
-      </label>
-      {/* <div className="errors">{errors?.password}</div> */}
-      <label>
-        <span>Password</span>
+        {/* </label> */}
+        <div className="errors">{errors?.password}</div>
+        {/* <label>
+          <span>Password</span> */}
         <input
+          className={`loginField`}
           type="password"
           value={password}
           onChange={update("password")}
           placeholder="Password"
         />
-      </label>
-      <div className="errors">
-        {password !== password2 && "Confirm Password field must match"}
-      </div>
-      <label>
-        <span>Confirm Password</span>
+        {/* </label> */}
+        <div className="errors">
+          {password !== password2 && "Confirm Password field must match"}
+        </div>
+        {/* <label>
+          <span>Confirm Password</span> */}
         <input
+          className={`loginField`}
           type="password"
           value={password2}
           onChange={update("password2")}
           placeholder="Confirm Password"
         />
-      </label>
-      <input
-        type="submit"
-        value="Sign Up"
-        disabled={
-          !email ||
-          !username ||
-          !firstName ||
-          !lastName ||
-          !password ||
-          password !== password2
-        }
-      />
-    </form>
+        {/* </label> */}
+        <input
+          className={`btnPrimary btn demo`}
+          type="submit"
+          value="Sign Up"
+          disabled={
+            !email ||
+            !username ||
+            !firstName ||
+            !lastName ||
+            !password ||
+            password !== password2
+          }
+        />
+      </form>
+    </div>
   );
 }
 
