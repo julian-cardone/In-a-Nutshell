f39d56694d905bfdef70aff211227d61c71e36b6
Ethan Graham <ethangraham43@gmail.com>
2022-12-22 10:06:24 -0500

    working on deleting event

--- frontend/src/components/EventsIndex/EventCard.js
+++ frontend/src/components/EventsIndex/EventCard.js
@@ -1,10 +1,17 @@
 import './EventsIndex.css';
+import { deleteEvent } from '../../store/events';
+
 function EventCard({event}) {
+    const removeEvent = (e) => {
+        debugger;
+        deleteEvent(event.id);
+    }
     return (
         <>
         <div className="event-card">
             <h2 className="event-title">{event.title}</h2>
             <p className="event-description">{event.description}</p>
+            <button className="remove-event-card" type="submit"onClick={removeEvent}>Remove event</button>
         </div>
         </>
     )
