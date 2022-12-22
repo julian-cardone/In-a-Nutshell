51b28f76f878d225012b245ef2573d57586c3d64
rtexelm <texelist@gmail.com>
2022-12-22 11:53:30 -0500

    Sizing

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,44 +1,44 @@
-import { Link, useHistory } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { logout } from '../../store/session';
-import './navbar.css';
+import { Link, useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../../store/session";
+import "./navbar.css";
 
-function NavBar () {
+function NavBar() {
   // const loggedIn = useSelector(state => !!state.session.user);
   const dispatch = useDispatch();
   const history = useHistory();
 
-  const logoutUser = e => {
-      e.preventDefault();
+  const logoutUser = (e) => {
+    e.preventDefault();
 
-      history.replace({ pathname: '/', state: { isActive: true }})
-      dispatch(logout());
-  }
+    history.replace({ pathname: "/", state: { isActive: true } });
+    dispatch(logout());
+  };
 
   const getLinks = () => {
     if (true) {
       return (
         <div className="links-nav">
-          <Link to={'/events'}>All Events</Link>
-          <Link to={'/profile'}>Profile</Link>
-          <Link to={'/events/new'}>Make an Event</Link>
+          <Link to={"/events"}>All Events</Link>
+          <Link to={"/profile"}>Profile</Link>
+          <Link to={"/events/new"}>Make an Event</Link>
           <button onClick={logoutUser}>Logout</button>
         </div>
       );
     } else {
       return (
         <div className="links-auth">
-          <Link to={'/signup'}>Signup</Link>
-          <Link to={'/login'}>Login</Link>
+          <Link to={"/signup"}>Signup</Link>
+          <Link to={"/login"}>Login</Link>
         </div>
       );
     }
-  }
+  };
 
   return (
     <>
       {/* <h1>In A Nutshell</h1> */}
-      { getLinks() }
+      {getLinks()}
     </>
   );
 }
