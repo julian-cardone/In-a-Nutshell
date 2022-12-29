53e4d501e301ec937ac24e2376ed91ca12659af6
Anthony_Chiodi <adc8wlc@gmail.com>
2022-12-29 16:58:20 -0500

    fix time issue on backend

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -1,8 +1,6 @@
 const { response, json } = require("express");
 const express = require("express");
 const router = express.Router();
-
-
 const Event = require("../../models/Event");
 
 router.get("/test", function (req, res, next) {
@@ -20,25 +18,33 @@ router.get("/", async (req, res, next) => {
 
 router.get("/:id", async (req, res, next) => {
   try {
-    const event = await Event.findOne({ _id: req.params.id })
-    res.send(event)
-  } catch(err) {
-    next(err)
+    const event = await Event.findOne({ _id: req.params.id });
+    res.send(event);
+  } catch (err) {
+    next(err);
   }
 });
 
 router.post("/new", async (req, res, next) => {
-  const newEvent = new Event({
+
+  let newEvent = new Event({
     title: req.body.event.title,
     description: req.body.event.description,
     eventDate: req.body.event.nyTime,
     status: req.body.event.status,
   });
 
+  let offset = newEvent.eventDate.getTimezoneOffset();
+  let time = newEvent.eventDate.getTime();
+  let dateTime = new Date( time - (offset * 60000))
+  newEvent.eventDate = dateTime
+
   try {
+    // console.log(newEvent);
     const savedEvent = await newEvent.save();
     res.json(newEvent);
   } catch (error) {
+    console.log(error);
     res.send(error);
   }
 });
