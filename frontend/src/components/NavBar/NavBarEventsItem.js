25b7d1604c9a76d61592cd5610d39704e0ae5c88
Julian Cardone <juliancardone1@gmail.com>
2023-01-10 11:45:48 -0500

    events index item sorta working

--- frontend/src/components/NavBar/NavBarEventsItem.js
+++ frontend/src/components/NavBar/NavBarEventsItem.js
@@ -1,4 +1,18 @@
-function NavBarEventsItem(){
+function NavBarEventsItem( { events } ){
+
+  console.log(events);
+
+  return (
+    events.map((event)=>{
+      return(
+        <>
+          <div className="event-container-default-nav">
+            {event.title}
+          </div>
+        </>
+      )
+    })
+  )
 
 }
 
