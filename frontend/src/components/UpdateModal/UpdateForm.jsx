7a4b8616591746e12e8eee25f393089d43675344
Anthony_Chiodi <adc8wlc@gmail.com>
2023-01-11 16:28:05 -0500

    style update modal

--- frontend/src/components/UpdateModal/UpdateForm.jsx
+++ frontend/src/components/UpdateModal/UpdateForm.jsx
@@ -15,15 +15,14 @@ function UpdateForm({ event, showModal, setShowModal, setEventsInd }) {
   const [title, setTitle] = useState(event.title);
   const [description, setDescription] = useState(event.description);
   const [eventDate, setEventDate] = useState(event.eventDate);
-  const [status, setStatus] = useState(false);
   const dispatch = useDispatch();
   const errors = useSelector((state) => state.errors.events);
-  const newEvent = useSelector((state) => state.events.new);
   const datething = new Date(event.eventDate)
   const [startDate, setStartDate] = useState(
-    // setHours(setMinutes(new Date(), 30), 16)
     datething
   );
+  // const localTime = formatInTimeZone(eventDate, 'America/New_York', 'yyyy-MM-dd HH:mm:ss zzz')
+
   const eventInfo = useContext(EventContext);
 
   useEffect(() => {
@@ -31,12 +30,7 @@ function UpdateForm({ event, showModal, setShowModal, setEventsInd }) {
     return () => dispatch(clearEventErrors());
   }, [dispatch, event.eventDate]);
 
-  // debugger
-
-  // const nyTime = formatInTimeZone(eventDate, 'America/New_York', 'yyyy-MM-dd HH:mm:ss zzz')
   let nyTime = startDate
-
-  // console.log(nyTime)
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -48,22 +42,11 @@ function UpdateForm({ event, showModal, setShowModal, setEventsInd }) {
     };
 
     setEventsInd("literally anything")
-    // debugger
     dispatch(updateEvent(changedEvent));
     setShowModal(false);
     eventInfo.eventInfo[1]()
   };
   setEventsInd("literally anything else")
-
-  const minutes = [];
-
-  for (let i = 0; i < 60; i++) {
-    if (i < 10) {
-      minutes.push(`0${i}`);
-    } else {
-      minutes.push(i);
-    }
-  }
 
   const update = (field) => {
     let setState;
@@ -84,24 +67,6 @@ function UpdateForm({ event, showModal, setShowModal, setEventsInd }) {
 
     return (e) => setState(e.currentTarget.value);
   };
-
-  // function handleHoursTwo(e) {
-  //   e.preventDefault();
-  //   setEventDate(setHours(new Date(eventDate), e.target.value));
-  // }
-
-  // function handleMinutesTwo(e) {
-  //   // console.log(e.target.value);
-  //   setEventDate(setMinutes(new Date(eventDate), e.target.value));
-  // }
-
-  function handleTime(e) {
-    let time = e.target.value
-    let hour = time.slice(0, 2)
-    let minute = time.slice(3, 6)
-   setEventDate(setHours(new Date(event.eventDate), e.target.value));
-   setEventDate(setMinutes(new Date(event.eventDate), e.target.value));
-  }
 
   return (
     <>
@@ -127,15 +92,17 @@ function UpdateForm({ event, showModal, setShowModal, setEventsInd }) {
           ></textarea>
           <div className={`timeMenu`}>
             <p>Select Time</p>
-
-            <DatePicker
-              style={{width: "1000px"}}
-              selected={startDate}
-              onChange={(date) => setStartDate(date)}
-              showTimeSelect
-              dateFormat="MMMM d, yyyy h:mm aa"
-            />
-
+            <div className="date-picker-container" style={{width: "100px"}}>
+              <DatePicker
+                className="date-picker"
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+                showTimeSelect
+                timeIntervals={5}
+                minDate = {new Date()}
+                dateFormat="MMMM d, yyyy h:mm aa"
+              />
+            </div>
           </div>
           <div className="errors">{errors && errors.event}</div>
           <input
