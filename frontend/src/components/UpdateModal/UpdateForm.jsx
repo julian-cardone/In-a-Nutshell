23d600a482e9442f71478b4f7fa7519e27c5c9ad
Anthony_Chiodi <adc8wlc@gmail.com>
2023-01-11 14:35:25 -0500

    worked out the update feature

--- frontend/src/components/UpdateModal/UpdateForm.jsx
+++ frontend/src/components/UpdateModal/UpdateForm.jsx
@@ -6,6 +6,8 @@ import { getDay, getMonth, setHours, setMinutes } from "date-fns";
 import { zonedTimeToUtc, formatInTimeZone } from 'date-fns-tz/esm'
 import { EventContext } from "../../App";
 import DatePicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css";
+
 
 
 
@@ -17,7 +19,11 @@ function UpdateForm({ event, showModal, setShowModal, setEventsInd }) {
   const dispatch = useDispatch();
   const errors = useSelector((state) => state.errors.events);
   const newEvent = useSelector((state) => state.events.new);
-  const [startDate, setStartDate] = useState(new Date());
+  const datething = new Date(event.eventDate)
+  const [startDate, setStartDate] = useState(
+    // setHours(setMinutes(new Date(), 30), 16)
+    datething
+  );
   const eventInfo = useContext(EventContext);
 
   useEffect(() => {
@@ -25,14 +31,14 @@ function UpdateForm({ event, showModal, setShowModal, setEventsInd }) {
     return () => dispatch(clearEventErrors());
   }, [dispatch, event.eventDate]);
 
+  // debugger
 
+  // const nyTime = formatInTimeZone(eventDate, 'America/New_York', 'yyyy-MM-dd HH:mm:ss zzz')
+  let nyTime = startDate
 
-  const nyTime = formatInTimeZone(eventDate, 'America/New_York', 'yyyy-MM-dd HH:mm:ss zzz')
-  console.log(eventDate)
   // console.log(nyTime)
 
   const handleSubmit = (e) => {
-    debugger
     e.preventDefault();
     const changedEvent = {
       id: event._id,
@@ -121,16 +127,15 @@ function UpdateForm({ event, showModal, setShowModal, setEventsInd }) {
           ></textarea>
           <div className={`timeMenu`}>
             <p>Select Time</p>
-            {/* <input type="datetime-local" onChange={e => handleTime(e)}></input> */}
+
             <DatePicker
-      selected={startDate}
-      onChange={(date) => setStartDate(date)}
-      showTimeSelect
-      timeFormat="HH:mm"
-      timeIntervals={15}
-      timeCaption="time"
-      dateFormat="MMMM d, yyyy h:mm aa"
-    />
+              style={{width: "1000px"}}
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+              showTimeSelect
+              dateFormat="MMMM d, yyyy h:mm aa"
+            />
+
           </div>
           <div className="errors">{errors && errors.event}</div>
           <input
