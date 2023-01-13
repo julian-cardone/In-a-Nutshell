0a60307d8374886417fcbda67de3144c8c5d354d
rtexelm <texelist@gmail.com>
2023-01-12 20:14:46 -0500

    Style events Index page, edit nav styling

--- frontend/src/components/EventsIndex/EventCard.js
+++ frontend/src/components/EventsIndex/EventCard.js
@@ -1,85 +1,78 @@
-import './EventsIndex.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { deleteEvent, updateEvent } from '../../store/events';
-import { useHistory } from 'react-router-dom';
-import { useState, useEffect, useContext } from 'react';
+import "./EventsIndex.css";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteEvent, updateEvent } from "../../store/events";
+import { useHistory } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
 import UpdateModal from "../UpdateModal/UpdateModal";
 import { EventContext } from "../../App";
 import { format, addMinutes } from "date-fns";
 
-function EventCard({event, setEventsInd}) {
-    const dispatch = useDispatch();
-    const history = useHistory();  
-    const eventInfo = useContext(EventContext);
-    const [showModal, setShowModal] = useState(false);
-    const removeEvent = () => {
-        dispatch(deleteEvent(event._id));
-        setEventsInd(null);
-      };
+function EventCard({ event, setEventsInd }) {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const eventInfo = useContext(EventContext);
+  const [showModal, setShowModal] = useState(false);
+  const removeEvent = () => {
+    dispatch(deleteEvent(event._id));
+    setEventsInd(null);
+  };
 
+  const [description, setDescription] = useState("");
+  const [eventDate, setEventDate] = useState(new Date());
+  const [status, setStatus] = useState(false);
+  const errors = useSelector((state) => state.errors.events);
 
-    const [description, setDescription] = useState('')
-    const [eventDate, setEventDate] = useState( new Date())
-    const [status, setStatus] = useState(false)
-    const errors = useSelector(state => state.errors.events);
+  const updateClick = (e) => {
+    e.preventDefault();
+    setShowModal(true);
+  };
 
-    const updateClick = (e) => {
-        e.preventDefault();
-        setShowModal(true);
-    }
+  const handleClick = (event) => {
+    console.log(event);
+    eventInfo.eventInfo[1](event);
+  };
 
-    const handleClick = (event) =>{
-      console.log(event);
-      eventInfo.eventInfo[1](event);
-    }
-
-    return (
-        <>
-        <div className="event-card"onClick={()=> handleClick(event)}>
-            <h2 className="event-title">{event.title}</h2>
-            <p className="event-description">{event.description}</p>
-            <div className='event-card-date'>
-            <p>
-                    {format(
-                      addMinutes(
-                        new Date(event.eventDate),
-                        new Date(
-                          event.eventDate
-                        ).getTimezoneOffset()
-                      ),
-                      "p"
-                    )}
-                    {/* {new Date(eventInfo.eventInfo[0].eventDate).getTimezoneOffset()} */}
-                  </p>
-                  <p>
-                    {format(new Date(event.eventDate), "eeee")}
-                    ,{" "}
-                    {format(
-                      new Date(event.eventDate),
-                      "MMMM do"
-                    )}{" "}
-                  </p>
-                </div>
-                <div className='index-container'>
-                <span className="updateButton p4" onClick={updateClick}>
-                    Update Event
-                  </span>
-                  <span className="deleteButton p4" onClick={removeEvent}>
-                    Delete Event
-                  </span>
-                </div>
+  return (
+    <>
+      <div className="event-card" onClick={() => handleClick(event)}>
+        <h2 className="event-title">{event.title}</h2>
+        <p className="event-description">{event.description}</p>
+        <div className="event-card-date">
+          <p className="h5">
+            {format(
+              addMinutes(
+                new Date(event.eventDate),
+                new Date(event.eventDate).getTimezoneOffset()
+              ),
+              "p"
+            )}
+            {/* {new Date(eventInfo.eventInfo[0].eventDate).getTimezoneOffset()} */}
+          </p>
+          <p className="sp1">
+            {format(new Date(event.eventDate), "eeee")},{" "}
+            {format(new Date(event.eventDate), "MMMM do")}{" "}
+          </p>
         </div>
-        {showModal && (
-            <UpdateModal
-              event={event}
-              setEventsInd={setEventsInd}
-              showModal={showModal}
-              setShowModal={setShowModal}
-              onClose={() => setShowModal(false)}
-            />
-          )}
-        </>
-    )
+        <div className="index-container">
+          <span className="updateButton p4" onClick={updateClick}>
+            Update Event
+          </span>
+          <span className="deleteButton p4" onClick={removeEvent}>
+            Delete Event
+          </span>
+        </div>
+      </div>
+      {showModal && (
+        <UpdateModal
+          event={event}
+          setEventsInd={setEventsInd}
+          showModal={showModal}
+          setShowModal={setShowModal}
+          onClose={() => setShowModal(false)}
+        />
+      )}
+    </>
+  );
 }
 
 export default EventCard;
--- frontend/src/components/EventsIndex/index.js
+++ frontend/src/components/EventsIndex/index.js
@@ -1,34 +1,45 @@
 import { useContext, useEffect } from "react";
-import EventCard from "./EventCard"
+import EventCard from "./EventCard";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchEvents } from "../../store/events";
-import './EventsIndex.css';
+import "./EventsIndex.css";
 import { EventContext } from "../../App";
-
-
+import backArrow from "../../assets/arrows/back-arrow.png";
+import { useHistory } from "react-router-dom";
 
 function EventsIndex({ setEventsInd }) {
-    const eventInfo = useContext(EventContext);
-    const events = eventInfo.eventInfo[2];
-    // const events = useSelector(state => Object.values(state.events.all));
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(fetchEvents());
-    }, [])
-    return (
+  const eventInfo = useContext(EventContext);
+  const events = eventInfo.eventInfo[2];
+  const history = useHistory();
+  const handleReturn = () => {
+    history.replace("/home");
+  };
+  // const events = useSelector(state => Object.values(state.events.all));
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchEvents());
+  }, []);
+  return (
     <>
-    <div className="outer-events-div" >
-     
-    <h1 className="title-for-events">All Events</h1>
-    <div className="events-div" >
-    
-    {events.map(event => (
-       <EventCard key={event.id} event={event} setEventsInd={ setEventsInd }/>
-    ))}
-    </div>    
-    </div>  
+      <div className="outer-events-div">
+        <p className="backToCalendar p4" onClick={handleReturn}>
+          <img src={backArrow} alt="" width="12" /> Calendar
+        </p>
+        <div className="header-for-events">
+          <h1 className="title-for-events">All Events</h1>
+        </div>
+        <div className="events-div">
+          {events.map((event) => (
+            <EventCard
+              key={event.id}
+              event={event}
+              setEventsInd={setEventsInd}
+            />
+          ))}
+        </div>
+      </div>
     </>
-    )
+  );
 }
 
 export default EventsIndex;
